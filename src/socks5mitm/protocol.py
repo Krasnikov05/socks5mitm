f6a4aa7a53dab78eb149cd62127fbf496ff5ecb5
Krasnikov Kirill <krasnikov.kirill@outlook.com>
2024-07-01 15:28:44 +0300

    Shorter docstring for protocol.py

--- src/socks5mitm/protocol.py
+++ src/socks5mitm/protocol.py
@@ -1,5 +1,4 @@
 class Socks5ProtocolError(Exception):
-    """Custom exception class for representing errors related to the SOCKS5 protocol.
-    This exception is raised when there is an issue with the SOCKS5 protocol implementation.
-    """
+    """Custom exception class for representing errors related to the SOCKS5 protocol."""
+
     pass
